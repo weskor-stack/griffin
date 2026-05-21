a5f9cd950b73a960376aafcfdefbec4afe279281
dperez-create <dperez@automatyco.com>
2026-05-21 13:06:20 -0600

    Se hace el merge de la rama main para volver a hacer commit de los cambios en conexion y la creacion del archio expiration time

--- conexion.py
+++ conexion.py
@@ -37,39 +37,6 @@ def obtener_datos_fila_unica():
         return None
     
 # def insert_simple(machine, process, operator, station):
-
-#Expiration time:
-def obtener_datos_expiration():
-    cursor = conn.cursor()
-    
-    try:
-        cursor.execute("SELECT * FROM configurador")
-        row_config = cursor.fetchone()
-
-        cursor.execute("SELECT * FROM expiration_time")
-        row_expiration = cursor.fetchone()
-
-        if row_config and row_expiration:            
-            datos = {
-                "workStation_ID": row_config[9],              # station
-                "Client_id": row_config[6],                   # client_id
-                "operator_id": row_config[7],                 # operator
-                "dispensing_process_name_1": row_config[4],   # process name
-                "time_defect_code_1": row_expiration[3],      # defect_code
-                "minute_duration_1": row_expiration[4],       # minute_duration
-                "move_to_loc_1": row_expiration[5]            # move_loc
-            }
-            return datos
-        else:
-            print("No se encontraron datos en configurador o expiration_time.")
-            return None
-
-    except Exception as e:
-        print(f"Error al consultar base de datos en obtener_datos_expiration: {e}")
-        return None
-    finally:
-        cursor.close()
-        conn.close()
 #URLs
 def get_station():
     try:
@@ -2596,6 +2563,39 @@ def get_expiration_time():
         print(f"[ERROR] get_expiration_time: {e}")
         return []
 
+
+#Expiration time:
+def obtener_datos_expiration():
+    cursor = conn.cursor()
+    
+    try:
+        cursor.execute("SELECT * FROM configurador")
+        row_config = cursor.fetchone()
+
+        cursor.execute("SELECT * FROM expiration_time")
+        row_expiration = cursor.fetchone()
+
+        if row_config and row_expiration:            
+            datos = {
+                "workStation_ID": row_config[9],              # station
+                "Client_id": row_config[6],                   # client_id
+                "operator_id": row_config[7],                 # operator
+                "dispensing_process_name_1": row_config[4],   # process name
+                "time_defect_code_1": row_expiration[3],      # defect_code
+                "minute_duration_1": row_expiration[4],       # minute_duration
+                "move_to_loc_1": row_expiration[5]            # move_loc
+            }
+            return datos
+        else:
+            print("No se encontraron datos en configurador o expiration_time.")
+            return None
+
+    except Exception as e:
+        print(f"Error al consultar base de datos en obtener_datos_expiration: {e}")
+        return None
+    finally:
+        cursor.close()
+        conn.close()
 ############################################################################################################################################################
 # name = "P1895152-00-G:SHG2242791000290"
 # parameters_pressfit(['F', '50', '10', '100', 'Numeric', 'N', 'PASSED', 'Comentarios', 'dwell_time'],name)
