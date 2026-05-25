6e35ce39c41dcb3fd633be8eb53d3b000293a77e
jcervantes-oss <jcervantes@automatyco.com>
2026-05-25 12:15:22 -0600

    se incluye Func. interlocking_station_50_80

--- interlocking_json.py
+++ interlocking_json.py
@@ -41,5 +41,43 @@ def interlocking_station_20(parent_serial_number, parent_part_number, heater_par
     # print(json.dumps(interlocking_station20, indent=4))
     return interlocking_station20
 
+def interlocking_station_50_80(parent_serial_number, parent_part_number, component_pn):
+    unit_information = []
+    configurador = conexion.configurador()
+    machine_id = configurador[0]
+    process_name = configurador[1]
+    operator = configurador[2]
+    station = configurador[3]
 
+    programas = conexion.select_programs()
+
+    for x in programas:
+        unit_information.append({
+            "name": "model_id",
+            "value": x[2]
+        })
+
+    unit_information.append({
+        "name": "station_id",
+        "value": machine_id
+    })
+
+    unit_information.append({
+        "name": "component_partnumber",
+        "value": component_pn
+    })
+
+    interlocking_st50_80 = {
+        "test_steps": {
+            "unit_information": unit_information
+        },
+        "serial": parent_serial_number,
+        "product": parent_part_number,
+        "station": station,
+        "operator": operator,
+        "process_name": process_name,
+        "location": ""
+    }
+    # print(json.dumps(interlocking_st50_80, indent=4))
+    return interlocking_st50_80
 # interlocking_station_20("AABB-parent_serial_number","CCGG02-parent_part_number","ZZXX01-heater_part_number")
