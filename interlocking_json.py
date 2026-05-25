7621673cbb9846bfbc7d60079d8a39d9d7761ff9
jcervantes-oss <jcervantes@automatyco.com>
2026-05-25 12:26:49 -0600

    se incluye linea 86

--- interlocking_json.py
+++ interlocking_json.py
@@ -51,16 +51,17 @@ def interlocking_station_50_80(parent_serial_number, parent_part_number, compone
 
     programas = conexion.select_programs()
 
+    # Orden correcto según PDF ST50/ST80 Sec 5.1: station_id → model_id → component_partnumber
+    unit_information.append({
+        "name": "station_id",
+        "value": machine_id
+    })
+
     for x in programas:
         unit_information.append({
             "name": "model_id",
             "value": x[2]
         })
-
-    unit_information.append({
-        "name": "station_id",
-        "value": machine_id
-    })
 
     unit_information.append({
         "name": "component_partnumber",
@@ -80,4 +81,6 @@ def interlocking_station_50_80(parent_serial_number, parent_part_number, compone
     }
     # print(json.dumps(interlocking_st50_80, indent=4))
     return interlocking_st50_80
+
 # interlocking_station_20("AABB-parent_serial_number","CCGG02-parent_part_number","ZZXX01-heater_part_number")
+# interlocking_station_50_80("MODEL1-001-0000015", "2102110-00-C", "COMPONENT-1")
