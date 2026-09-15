4a6171bfcfb1685175dc2beb42e75126da2d1ae3
dperez-create <dperez@automatyco.com>
2026-09-15 09:29:07 -0600

    Se cambia formato de fecha de d/m/y a m/d/y

--- traceability_json.py
+++ traceability_json.py
@@ -1465,7 +1465,7 @@ def traceability_station_60_v2(serial_padre, defect_code_default=""):
         program_version = "default_program"
 
     now = datetime.now(ZoneInfo("America/Mexico_City"))
-    now_utc = now.strftime("%d-%m-%Y %I:%M:%S %p")
+    now_utc = now.strftime("%m-%d-%Y %I:%M:%S %p")
 
     fecha = str(parte[3])
 
@@ -1473,7 +1473,7 @@ def traceability_station_60_v2(serial_padre, defect_code_default=""):
     fecha_dt = datetime.strptime(fecha, "%Y-%m-%d %H:%M:%S")
 
     # Dar el formato deseado
-    fecha_formateada = fecha_dt.strftime("%d-%m-%Y %H:%M:%S %p")
+    fecha_formateada = fecha_dt.strftime("%m-%d-%Y %H:%M:%S %p")
 
     # ==========================================================
     # OBTENER ATRIBUTOS
